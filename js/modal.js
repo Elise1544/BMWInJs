908aaf95efb12e01dc119d766221158c56f576a1
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2021-05-20 14:40:18 +0300

    Задание 3

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+const moreElems = document.querySelectorAll('.more');
+const modalElem = document.querySelector('.modal');
+
+const openModal = () => {
+  modalElem.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modalElem.classList.add('hidden');
+};
+
+moreElems.forEach((moreElem) => {
+  moreElem.addEventListener('click', openModal);
+});
+
+modalElem.addEventListener('click', (evt) => {
+  const target = evt.target;
+  if (target.classList.contains('overlay') || target.classList.contains('modal__close')) {
+    closeModal();
+  }
+});
